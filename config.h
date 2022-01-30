3802dd7443b192d12f842f611edaaebcfb15dad0
i9p <55615254+i9p@users.noreply.github.com>
2022-01-30 18:14:47 +1100

    float always on top + screenshot

--- config.h
+++ config.h
@@ -57,11 +57,20 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+  { "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
-  { "[@]",      spiral },
- 	{ "[\\]",      dwindle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -75,20 +84,31 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+/* statuscmd setup */
 #define STATUSBAR "dwmblocks"
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "", NULL };
-static const char *launchercmd[] = { "launcher", NULL };
-static const char *powermenucmd[] = { "powermenu", NULL };
-static const char *killcmd[] = { "pkill dwm", NULL };
-static const char *emojicmd[] = { "rofi -show emoji -modi emoji -theme ~/.config/rofi/emoji/emoji.rasi", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *launchercmd[] = { "launcher", NULL }; /* bound to super+space */
+static const char *powermenucmd[] = { "powermenu", NULL }; /* bound to super+shift+space */
+static const char *emojicmd[] = { "emojimenu", NULL }; /* bound to super+period */
+static const char *termcmd[]  = { "st", NULL }; /* bound to super+shift+enter */
+static const char *screenshotcmd[]  = { "flameshot gui", NULL }; /* bound to super+shift+enter */
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+
+  /* spawn functions */
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = launchercmd } },
+	{ MODKEY|ShiftMask,             XK_space,  spawn,          {.v = powermenucmd } },
+	{ MODKEY,                       XK_period, spawn,          {.v = emojicmd } },
+	{ NULL,                         XK_Print,  spawn,          {.v = screenshotcmd } },
+
+  /* window manipulation */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -98,19 +118,27 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
+
+  /* layouts */
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ControlMask,           XK_t,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[5]} },
+	{ MODKEY|ControlMask,           XK_r,      setlayout,      {.v = &layouts[6]} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[7]} },
+	{ MODKEY|ShiftMask,             XK_g,      setlayout,      {.v = &layouts[8]} },
+	{ MODKEY|ControlMask,           XK_g,      setlayout,      {.v = &layouts[9]} },
+	{ MODKEY|ControlMask|ShiftMask, XK_g,      setlayout,      {.v = &layouts[10]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[11]} },
+	{ MODKEY|ControlMask,           XK_m,      setlayout,      {.v = &layouts[12]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_c,      togglefloating, {0} },
-	{ MODKEY,                       XK_space,  spawn,          {.v = launchercmd } },
-	{ MODKEY|ShiftMask,             XK_space,  spawn,          {.v = powermenucmd } },
-	{ MODKEY,                       XK_period, spawn,          {.v = emojicmd } },
+
+  /* tag manipulation */
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	//{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	//{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
@@ -122,7 +150,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {0} },
+
+  /* monitor manipulation (multiple monitor setups) */
+	/* { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, */
+	/* { MODKEY,                       XK_period, focusmon,       {.i = +1 } }, */
 };
 
 /* button definitions */
