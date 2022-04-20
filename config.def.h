790c2865fd3bad8a1d047e2f47cb96b7f451c2c5
i9p <55615254+i9p@users.noreply.github.com>
2022-04-20 23:48:03 +1000

    compile without wal

--- config.def.h
+++ config.def.h
@@ -10,26 +10,40 @@ static const unsigned int gappov    = 12;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const char dmenufont[]       = "monospace:size=10";
+
+/* desktop */
+#if FALSE
+static const char *fonts[]          = { 
+  "monospace:size=10",
+  "feather:size=10",
+  "Noto Sans CJK JP:size=10",
+  "JoyPixels:size=9"
+};
+#else
 static const char *fonts[]          = { 
   "monospace:size=12",
   "feather:size=12",
   "Noto Sans CJK JP:size=12",
   "JoyPixels:size=11"
 };
-static const char dmenufont[]       = "monospace:size=10";
+#endif
 
-//#include "/home/i9p/.cache/wal/colors-wal-dwm.h"
-
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+/* use wal if available, fallback to defined colors */
+#define wal "/home/i9p/.cache/wal/colors-wal-dwm.h"
+#if __has_include(wal)
+#include wal
+#else
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_urgborder[]   = "#ff0000";
 static const char *colors[][3]      = {
-	*               fg         bg         border   *
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+   /*               fg         bg         border   */
+   [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+   [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+   [SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
+#endif
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
